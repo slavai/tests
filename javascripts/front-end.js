97bb6f36af23eb444324110282dee6d32100656b
Slava <isaevslava87@yandex.ua>
2013-11-20 21:59:54 +0200

    add Version 0.1

---
+++ javascripts/front-end.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function(){
+
+    $("#primari_navigation_list").mmenu({
+        // options object
+    }, {
+        // configuration object
+        listClass: "buttons"
+    });
+
+})

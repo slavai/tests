d7d95026f9531ceca5fdbd00629ff050ea4415bc
Slava <isaevslava87@yandex.ua>
2013-11-21 01:29:18 +0200

    push from dev.

--- javascripts/front-end.js
+++ javascripts/front-end.js
@@ -1,10 +1,21 @@
 jQuery(document).ready(function(){
 
-    $("#primari_navigation_list").mmenu({
-        // options object
-    }, {
-        // configuration object
-        listClass: "buttons"
+    var $menu = $('#menu');
+
+    //	Toggle menu
+    $('#open-icon-menu a').click(function( e ) {
+        e.stopImmediatePropagation();
+        e.preventDefault();
+        $menu.trigger( $menu.hasClass( 'mm-opened' ) ? 'close.mm' : 'open.mm' );
     });
+
+    //	Create the menu
+    $menu.mmenu({
+//        options:
+            position   : "right",
+            zposition  : "next"
+
+    });
+
 
 })
